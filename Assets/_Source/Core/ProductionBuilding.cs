cb55595493be2836f8c75dcf7b0bac2e9b13a643
Anastasia Stepanova <alan.kasper.51.16.9@gmail.com>
2023-11-14 17:55:58 +0300

    feat(visual): add sliders behind buttons

--- Assets/_Source/Core/ProductionBuilding.cs
+++ Assets/_Source/Core/ProductionBuilding.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _Source.Core
 {
@@ -7,9 +9,15 @@ namespace _Source.Core
         [SerializeField] private ResourceBank _resourceBank;
         [SerializeField] private int _increaseDelta = 1;
         [SerializeField] private GameResource _resource;
-    
+        [SerializeField] private double _productionTime = 2;
         public void IncreaseResource()
         {
+            StartCoroutine(Increase());
+        }
+
+        private IEnumerator Increase()
+        {
+            yield return new WaitForSeconds(2f);
             _resourceBank.ChangeResource(_resource, _increaseDelta);
         }
     }
